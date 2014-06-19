26db0b7aa245d70ea0f28ac15c0fd95f5bba9c12
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-06-19 08:50:58 +0100

    Move identifier up one level

--- cli/commands/test/runners/browserstack.js
+++ cli/commands/test/runners/browserstack.js
@@ -34,8 +34,8 @@ var beforeSuite = function(options, done) {
     var bin = getBrowserStackLocalBin(options)
     var binOptions = [
         options.browserstack.key,
-        'localhost,' + options.applicationPort + ',0',
-        '-localIdentifier="' + identifier + '"'
+        '-localIdentifier="' + identifier + '"',
+        'localhost,' + options.applicationPort + ',0'
     ]
     var identifier = Math.random().toString(36).replace(/[^a-z]+/g, '')
     childProcess = spawn(bin, binOptions)
