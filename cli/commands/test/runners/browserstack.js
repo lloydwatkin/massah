8e1bb09baa9b156508025bfb1cdbde0c86493f8c
Lloyd Watkin <lloyd.watkin@surevine.com>
2014-10-07 10:58:44 +0100

    Run browserstack local in a more secure manner

--- cli/commands/test/runners/browserstack.js
+++ cli/commands/test/runners/browserstack.js
@@ -42,6 +42,7 @@ var beforeSuite = function(options, done) {
         process.exit(1)
     }
     var binOptions = [
+        ' -only ',
         /* '-localIdentifier="' + identifier + '" ', */
         options.browserstack.key,
         'localhost,' + options.applicationPort + ',0'
